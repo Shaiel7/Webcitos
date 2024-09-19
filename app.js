5f7b1b613177cec4db440d7b4c608297db523b85
Shaiel7 <shaielalmendraaranaflores@gmail.com>
2024-09-19 12:44:48 -0500

    Update

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para servir el archivo HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'LIECAR.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+<script src="/scripts.js"></script>
